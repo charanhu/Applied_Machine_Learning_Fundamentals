d68ee1792828d7f7a848bd7b88f6cbebdb9906dc
charanhu <charanhumail@gmail.com>
2022-10-12 22:19:11 +0530

    ver 14

--- _build/jupyter_execute/Chapter1/python_comments.py
+++ _build/jupyter_execute/Chapter1/python_comments.py
@@ -3,8 +3,11 @@
 
 # # Python Comments
 
+# Python Comments are used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code. Comments can be used to add a description to the code.
+
 # In[1]:
 
 
 # Python Comments
+# Python Comments are used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code. Comments can be used to add a description to the code.
 
