fea501c869670d861b949a54636fb1d516162120
charanhu <charanhumail@gmail.com>
2022-10-12 12:55:50 +0530

    ver 2

---
+++ _build/jupyter_execute/Chapter1/python_introduction.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Introduction
+
+# In[1]:
+
+
+# Python Introduction
+# Python is a high-level, interpreted, interactive and object-oriented scripting language.
+
